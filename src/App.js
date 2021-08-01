09bb8758c3ac1fe5728e2354862986eeadd82fbc
franklin rodriguez <franklinserif@gmail.com>
2021-08-01 13:23:52 -0400

    add colorpicker desktop and mobile version and react-device-detect

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
+import { useState } from "react";
+import { PhotoshopPicker, SketchPicker } from "react-color";
+import { isDesktop } from "react-device-detect";
+
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  const [color, setColor] = useState("#334756");
+
+  return (
+    <div className="App">
+      <header className="header">
+        {isDesktop ? (
+          <PhotoshopPicker
+            className="colorpicker"
+            color={color}
+            onChange={(updateColor) => setColor(updateColor.hex)}
+          />
+        ) : (
+          <SketchPicker
+            className="colorpicker"
+            color={color}
+            onChange={(updateColor) => setColor(updateColor.hex)}
+          />
+        )}
+      </header>
+      <main className="main"></main>
+    </div>
+  );
 }
 
 export default App;
