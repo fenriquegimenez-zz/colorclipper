344d50f82c8b869b9da1c6bf92de4ccd7c6b6bc4
Enrique Gimenez <fenriquegimenez@gmail.com>
2021-08-02 12:59:29 -0400

    Added the document title

--- src/App.js
+++ src/App.js
@@ -1,33 +1,38 @@
-import { useState } from "react";
-import ColorPicker from "./components/ColorPicker";
-import ColorPalette from "./components/ColorPalette";
-import TheoryColor from "./components/TheoryColor";
-import colorGenerator from "./utitly/ColorGenerator";
-import Values from "values.js";
-import { FaGithub, FaTwitter } from "react-icons/fa";
-import Logo from "./logo.svg";
+import { useState, useEffect } from "react"
+import ColorPicker from "./components/ColorPicker"
+import ColorPalette from "./components/ColorPalette"
+import TheoryColor from "./components/TheoryColor"
+import colorGenerator from "./utitly/ColorGenerator"
+import Values from "values.js"
+import { FaGithub, FaTwitter } from "react-icons/fa"
+import Logo from "./logo.svg"
 
-import "./App.css";
+import "./App.css"
 
 function App() {
-  const [currentColor, setCurrentColor] = useState("#4358E4");
-  const [colors, setColors] = useState(colorGenerator("triad", "#4358E4"));
-  const [theory, setTheory] = useState("triad");
+  const [currentColor, setCurrentColor] = useState("#4358E4")
+  const [colors, setColors] = useState(colorGenerator("triad", "#4358E4"))
+  const [theory, setTheory] = useState("triad")
   const [fullColors, setFullColors] = useState(
-    new Values("#4358E4").all(5).map((color) => color.rgb.join(","))
-  );
-  const handleOnChange = (color) => {
-    setCurrentColor(color.hex);
-    setColors(colorGenerator(theory, currentColor));
-    setFullColors(
-      new Values(currentColor).all(5).map((color) => color.rgb.join(","))
-    );
-  };
+    new Values("#4358E4").all(5).map(color => color.rgb.join(","))
+  )
 
-  const handleTheoryOptions = (option) => {
-    setTheory(option);
-    setColors(colorGenerator(option, currentColor));
-  };
+  useEffect(() => {
+    document.title = "Color Clipper"
+  }, [])
+
+  const handleOnChange = color => {
+    setCurrentColor(color.hex)
+    setColors(colorGenerator(theory, currentColor))
+    setFullColors(
+      new Values(currentColor).all(5).map(color => color.rgb.join(","))
+    )
+  }
+
+  const handleTheoryOptions = option => {
+    setTheory(option)
+    setColors(colorGenerator(option, currentColor))
+  }
 
   return (
     <div className="App">
@@ -95,7 +100,7 @@ function App() {
         </section>
       </footer>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/ColorPalette.jsx
+++ src/components/ColorPalette.jsx
@@ -1,17 +1,16 @@
-import { useState, useEffect } from "react";
-import Color from "./Color";
+import Color from "./Color"
 
-import "./css/colorpalette.css";
+import "./css/colorpalette.css"
 
 const ColorPalette = ({ colors, rgb }) => {
-  let currentColors = colors;
+  let currentColors = colors
   if (rgb === true) {
-    currentColors = colors.map((color) => `rgb(${color})`);
+    currentColors = colors.map(color => `rgb(${color})`)
   }
 
   const listOfColors = currentColors.map((color, index) => (
     <Color color={color} key={index + new Date().getUTCMilliseconds()} />
-  ));
-  return <section className="colorpalette">{listOfColors}</section>;
-};
-export default ColorPalette;
+  ))
+  return <section className="colorpalette">{listOfColors}</section>
+}
+export default ColorPalette
